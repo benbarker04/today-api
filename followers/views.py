97a8ba4b83a109332e81bf5b40ad54d6e786bfeb
Ben Barker <benbarker074@gmail.com>
2024-06-28 13:24:27 +0000

    added all things needed for api such as all list and detail views, all functions and JSON web tokens and added all routes and filters

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from today_api.permissions import IsOwnerOrReadOnly
+from .models import Followers
+from .serializers import FollowerSerializer
 
-# Create your views here.
+class FollowerList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Followers.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class FollowersDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Followers.objects.all()
