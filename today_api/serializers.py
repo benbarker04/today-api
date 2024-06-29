87150673adac5ccd08d91296783ee2fabdef411a
Ben Barker <benbarker074@gmail.com>
2024-06-29 08:59:31 +0000

    add serializers.py in 'today_api' folder

--- today_api/serializers.py
+++ today_api/serializers.py
@@ -1,20 +1,12 @@
-from rest_framework import generics, permissions
-from django_filters.rest_framework import DjangoFilterBackend
-from today_api.permissions import IsOwnerOrReadOnly
-from .models import Comment
-from .serializers import CommentSerializer, CommentDetailSerializer
+from dj_rest_auth.serializers import UserDetailsSerializer
+from rest_framework import serializers
 
-class CommentList(generics.ListCreateAPIView):
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Comment.objects.all()
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['post']
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+class CurrentUserSerializer(UserDetailsSerializer):
+    profile_id = serializers.ReadOnlyField(source='profile.id')
+    profile_image = serializers.ReadOnlyField(source='profile.image.url')
 
-class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = CommentDetailSerializer
-    queryset =Comment.objects.all()
+    class Meta(UserDetailsSerializer.Meta):
+        fields = UserDetailsSerializer.Meta.fields + (
+            'profile_id', 'profile_image'
+        )
--- today_api/settings.py
+++ today_api/settings.py
@@ -121,7 +121,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
